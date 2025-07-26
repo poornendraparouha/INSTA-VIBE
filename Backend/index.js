970e087aa6c494e692c150ec146bf16b917e5f89
poornendra parouha <poornendraparouha@gmail.com>
2025-07-26 23:38:30 +0530

    connected database

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import connectDB from './utility/db.js';
+dotenv.config({});
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to the Express server!",
+    success: true,
+  });
+});
+
+// Middlewares
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
+
+ 
