437c553132ebacb9d106019ed8adaf6322e21a7d
poornendra parouha <poornendraparouha@gmail.com>
2025-07-27 00:20:58 +0530

    created models

---
+++ Backend/models/conversation..model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema({
+    participants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }],
+    message: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+        required: true
+    }]
+}, { timestamps: true });
+
+export default Conversation = mongoose.model("Conversation", conversationSchema);
---
+++ Backend/models/post.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+
+    caption: {
+        type: String,
+        default: "",
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    comments: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "comment"
+        }
+    }],
+}, {timestamps: true});
+
+export const Post = mongoose.model("Post", postSchema);
