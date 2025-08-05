b32d8f86a18ce6a6623dfd2bc3fff4770323b7aa
poornendra parouha <poornendraparouha@gmail.com>
2025-08-05 19:17:37 +0530

    set up persistor so that the profile picture retains when page refreshed

--- Frontend/src/components/LeftSidebar.jsx
+++ Frontend/src/components/LeftSidebar.jsx
@@ -1,49 +1,62 @@
-import {Heart, Home, LogOutIcon, MessageCircle, PlusSquare, Search, Sidebar, TrendingUp, } from "lucide-react";
+import {
+	Heart,
+	Home,
+	LogOutIcon,
+	MessageCircle,
+	PlusSquare,
+	Search,
+	Sidebar,
+	TrendingUp,
+} from "lucide-react";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import logo from "/instagram.png";
 import { toast } from "sonner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
-const sidebarItems = [
-	{ icon: <Home />, text: "Home" },
-	{ icon: <Search />, text: "Search" },
-	{ icon: <TrendingUp />, text: "Explore" },
-	{ icon: <MessageCircle />, text: "Messages" },
-	{ icon: <Heart />, text: "Notifications" },
-	{ icon: <PlusSquare />, text: "Create" },
-	{
-		icon: (
-			<Avatar>
-				<AvatarImage src="https://github.com/shadcn.png" />
-				<AvatarFallback>CN</AvatarFallback>
-			</Avatar>
-		),
-		text: "Profile",
-	},
-	{ icon: <LogOutIcon />, text: "Logout" },
-];
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthUser } from "@/redux/authSlice";
 
 function LeftSidebar() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
+	const { user } = useSelector((store) => store.auth);
+	const dispatch = useDispatch();
 
 	const logoutHandler = async () => {
 		try {
 			const res = await axios.get("http://localhost:8000/api/v1/user/logout", {
 				withCredentials: true,
 			});
-            if(res.data.success){
-                navigate('/login');
-                toast.success(res.data.message)
-            }
+			if (res.data.success) {
+				dispatch(setAuthUser(null))
+				navigate("/login");
+				toast.success(res.data.message);
+			}
 		} catch (error) {
 			toast.error(error.response.data.message);
 		}
 	};
-    const sidebarHandler = (textType) => {
-        if(textType === "Logout") logoutHandler();
-    }
+	const sidebarHandler = (textType) => {
+		if (textType === "Logout") logoutHandler();
+	};
+	const sidebarItems = [
+		{ icon: <Home />, text: "Home" },
+		{ icon: <Search />, text: "Search" },
+		{ icon: <TrendingUp />, text: "Explore" },
+		{ icon: <MessageCircle />, text: "Messages" },
+		{ icon: <Heart />, text: "Notifications" },
+		{ icon: <PlusSquare />, text: "Create" },
+		{
+			icon: (
+				<Avatar>
+					<AvatarImage src={user?.profilePicture} alt="profile_image" />
+					<AvatarFallback>CN</AvatarFallback>
+				</Avatar>
+			),
+			text: "Profile",
+		},
+		{ icon: <LogOutIcon />, text: "Logout" },
+	];
 	return (
 		<div className="fixed top-0 left-0 h-screen w-[16%] min-w-[200px] bg-white border-r border-gray-200 shadow-md px-4 py-6 hidden sm:flex flex-col justify-between z-10">
 			<div className="flex flex-col gap-6">
@@ -54,10 +67,14 @@ function LeftSidebar() {
 
 				{sidebarItems.map((item, index) => {
 					return (
-						<div onClick={()=>sidebarHandler(item.text)} key={index} className="flex items-center gap-3 px-3 py-2 hover:bg-gray-100 rounded-lg transition-colors cursor-pointer relative">
+						<div
+							onClick={() => sidebarHandler(item.text)}
+							key={index}
+							className="flex items-center gap-3 px-3 py-2 hover:bg-gray-100 rounded-lg transition-colors cursor-pointer relative"
+						>
 							{item.icon}
 							<span className="text-sm font-medium text-gray-800">
-							{item.text}
+								{item.text}
 							</span>
 						</div>
 					);
--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,8 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
-const store = configureStore({
-  reducer: {
+import {
+    persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+	key: "root",
+	version: 1,
+	storage,
+};
+const rootReducer = combineReducers({
     auth: authSlice,
-  }
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 export default store;
