e55ff4212e72fc7e97f4ec2db0125537306361c2
poornendra parouha <poornendraparouha@gmail.com>
2025-08-20 10:54:21 +0530

    updated URL

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-	origin: "http://localhost:5173",
+	origin: process.env.URL,
 	credentials: true,
 };
 app.use(cors(corsOptions));
