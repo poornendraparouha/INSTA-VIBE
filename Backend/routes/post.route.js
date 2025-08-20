6b08581002fdc963dbb217d6fd9813f9064ee324
poornendra parouha <poornendraparouha@gmail.com>
2025-08-20 14:12:52 +0530

    updated bookmark api method

--- Backend/routes/post.route.js
+++ Backend/routes/post.route.js
@@ -15,27 +15,14 @@ import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
-// Add New Post (with image)
 router.route("/addpost").post(isAuthenticated, upload.single("image"), addNewPost);
-
-// Get All Posts (Public)
 router.route("/allposts").get(getAllPost);
-
-// Get Posts by Logged-In User
 router.route("/userpost/all").get(isAuthenticated, getUserPost);
-
-// Like & Dislike a Post
 router.route("/:id/like").post(isAuthenticated, likePost);
 router.route("/:id/dislike").post(isAuthenticated, dislikePost);
-
-// Add and Get Comments for a Post
 router.route("/:id/comment").post(isAuthenticated, addComments);
 router.route("/:id/comment/all").get(getCommentsOfPost);
-
-// Bookmark & Unbookmark a Post
-router.route("/:id/bookmark").get(isAuthenticated, bookmarkPost);
-
-// Delete a Post (Author only)
+router.route("/:id/bookmark").post(isAuthenticated, bookmarkPost);
 router.route("/delete/:id").delete(isAuthenticated, deletePost);
 
 export default router;
